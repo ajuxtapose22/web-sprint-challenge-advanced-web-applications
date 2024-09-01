f7a9ae6fdebe3b6664df126ef32a821147ef622c
ajuxtapose22 <aaron.jardin@yahoo.com>
2024-09-01 16:48:32 -0700

    spinner test function implemented

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,17 @@
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
-test('sanity', () => {
-  expect(true).toBe(false)
+
+import React from "react"
+import { render, screen } from '@testing-library/react'
+import Spinner from "./Spinner"
+
+test('renders Spinner when ON prop is true ', () => {
+  render(<Spinner on={true} />)
+  expect(screen.getByText("Please Wait...")).toBeInTheDocument()
 })
+
+test('renders Spinner when ON prop is false ', () => {
+  render(<Spinner on={true} />)
+  expect(screen.queryByText("Please Wait...")).toBeNull()
+})
+
